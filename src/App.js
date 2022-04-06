14db9a03b3502237e03de9611527302369d04eb5
szang523 <60521351+szang523@users.noreply.github.com>
2022-04-06 16:15:47 -0400

    Refactored to make search box a component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React, { useState } from "react";
+import Search from "./components/Search/Search";
 
 function App() {
   const apiKey = "96b7e702087a5e96f30cc8a2e1e36891";
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className="container">
       <h1> Weather App </h1>
-      <input className="input" placeholder="Search city" />
+      <Search />
     </div>
   );
 }
---
+++ src/components/Search/Search.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Search = (props) => {
+  return (
+    <div>
+      <input type="text" className="input" placeholder="Search city" />
+    </div>
+  );
+};
+
+export default Search;
