50b3bea6227788a25b6336a52e6db86d27de80b0
szang523 <60521351+szang523@users.noreply.github.com>
2022-04-05 23:43:32 -0400

    imported useState & added apiKey

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const apiKey = "96b7e702087a5e96f30cc8a2e1e36891";
+
   return (
     <div>
       <h1> Weather App </h1>
