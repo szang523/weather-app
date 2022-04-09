44778bb18aa6f02e0cdd97b533a977a905b0cbe2
szang523 <60521351+szang523@users.noreply.github.com>
2022-04-08 22:35:01 -0400

    Created weather component placeholder/files

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 
 const Search = (props) => {
-  const apiKey = "96b7e702087a5e96f30cc8a2e1e36891";
+  // const apiKey = "96b7e702087a5e96f30cc8a2e1e36891";
   const [weatherData, setWeatherData] = useState([{}]);
   const [city, setCity] = useState("");
 
   const getWeather = (event) => {
     if (event.key === "Enter") {
       fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={apiKey}"
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
       )
         .then((response) => response.json())
         .then((data) => {
---
+++ src/components/Weather/Weather.js
@@ -0,0 +1,9 @@
+const Weather = (props) => {
+  return (
+    <div>
+      <p>Weather</p>
+    </div>
+  );
+};
+
+export default Weather;
